df14f361975155821c93babdb1d15925bd6f81ff
andrewdubin <andrewdubin@verizon.net>
2022-02-15 16:57:54 -0500

    Added metric calcs, date filtering

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 
 #Step 1: Import document, Change Program Start Date to Date of Identification
 file = input("File Name?: ")
-df = pd.read_csv(file)
+df = pd.read_csv(file,parse_dates=['Program Start Date','Program End Date'])
 df = df.rename(columns={"Program Start Date":"Date of Identification",'Case Number':'Client ID','Veteran Status (HUD)':'Veteran Status'})
 df = df.drop(["Relationship","Family Name",'Name'],axis=1)
 df = df.drop_duplicates()
@@ -55,18 +55,69 @@ df['1stDateofID'] = df['Client ID'].map(df.groupby('Client ID').agg({'Date of Id
 df['Return to Active Date'] = np.nan
 df.loc[(df['Client ID Household Counter']>1) & (df['1stDateofID']!=df['Date of Identification'])
 ,"Return to Active Date"] = df['Date of Identification']
+df['Return to Active Date'] = pd.to_datetime(df['Return to Active Date'])
 
-#Step 9, Output
+#Step 9, Calculate most recent move-in or inactive dates
+df['Most Recent Move-In Date'] = df['Client ID'].map(df.groupby('Client ID').agg({'Housing Move-In Date':'max'})['Housing Move-In Date'])
+df['Most Recent Inactive Date'] = df['Client ID'].map(df.groupby('Client ID').agg({'Inactive Date':'max'})['Inactive Date'])
+
+#Step 10, Narrow down dataframe down to active clients and newly exited clients for the reporting month
+reporting_year = input("Reporting Year? Enter four digits: ")
+reporting_month = input("Reporting Month? Enter month number: ")
+reporting_date = pd.to_datetime(reporting_year+reporting_month,format='%Y%m',errors='ignore')
+num_of_days = reporting_date.days_in_month
+dates = {}
+dates['Reporting Date'] = str(reporting_date.year)+'-'+ str(reporting_date.month) +'-'+ str(num_of_days)
+dates['Start Date'] = str(reporting_date.year)+'-'+ str(reporting_date.month) +'-1'
+
+active_df = df[(df['Date of Identification']<=dates['Reporting Date']) &
+((df['Program End Date'].isnull()==True) | (df['Program End Date']<df['Date of Identification']) | (df['Program End Date']>dates['Reporting Date']))]
+exited_df = df[(df['Program End Date']>=dates['Start Date']) & (df['Program End Date']<=dates['Reporting Date'])]
+filtered_df = pd.concat([active_df,exited_df])
+filtered_df = filtered_df.reset_index()
+filtered_df = filtered_df.drop(['index'],axis=1)
+
+#Step 11, Output
 output = input("Parsing Finished. Output File Name?: ")
-df.to_csv(output)
+filtered_df.to_csv(output)
 
-#Step 10, Determine clients that "No longer meets population criteria" by demographic info
+#Step 12, Determine clients that "No longer meets population criteria" by demographic info
 #All persons, all singles, veterans, chronic, chronic veteran, youth, families
-print("How many clients this month No longer meet population criteria?")
-print("All clients ",df['Dismissal Reason'].where(df['Dismissal Reason']=="No longer meets population criteria").count())
-print("Singles ",df['Dismissal Reason'].where((df['Dismissal Reason']=="No longer meets population criteria")&(df['Household Type']=='Single Adults')).count())
-print("Veterans ",df['Dismissal Reason'].where((df['Dismissal Reason']=="No longer meets population criteria")&(df['Veteran Status']=='Yes')).count())
-print("Chronic ",df['Dismissal Reason'].where((df['Dismissal Reason']=="No longer meets population criteria")&(df['Chronic Status']=='Yes')).count())
-print("Chronic Veterans ",df['Dismissal Reason'].where((df['Dismissal Reason']=="No longer meets population criteria")&(df['Chronic Status']=='Yes')&(df['Veteran Status']=='Yes')).count())
-print("Youth ",df['Dismissal Reason'].where((df['Dismissal Reason']=="No longer meets population criteria")&(df['Household Type']=='Youth')).count())
-print("Families ",df['Dismissal Reason'].where((df['Dismissal Reason']=="No longer meets population criteria")&(df['Household Type']=='Families')).count())
+print("\nHow many clients this month No longer meet population criteria?")
+print("All clients ",filtered_df['Dismissal Reason'].where(filtered_df['Dismissal Reason']=="No longer meets population criteria").count())
+print("Singles ",filtered_df['Dismissal Reason'].where((filtered_df['Dismissal Reason']=="No longer meets population criteria")&(filtered_df['Household Type']=='Single Adults')).count())
+print("Veterans ",filtered_df['Dismissal Reason'].where((filtered_df['Dismissal Reason']=="No longer meets population criteria")&(filtered_df['Veteran Status']=='Yes')).count())
+print("Chronic ",filtered_df['Dismissal Reason'].where((filtered_df['Dismissal Reason']=="No longer meets population criteria")&(filtered_df['Chronic Status']=='Yes')).count())
+print("Chronic Veterans ",filtered_df['Dismissal Reason'].where((filtered_df['Dismissal Reason']=="No longer meets population criteria")&(filtered_df['Chronic Status']=='Yes')&(filtered_df['Veteran Status']=='Yes')).count())
+print("Youth ",filtered_df['Dismissal Reason'].where((filtered_df['Dismissal Reason']=="No longer meets population criteria")&(filtered_df['Household Type']=='Youth')).count())
+print("Families ",filtered_df['Dismissal Reason'].where((filtered_df['Dismissal Reason']=="No longer meets population criteria")&(filtered_df['Household Type']=='Families')).count())
+
+#Step 13, Calculate BFZ Reporting Metrics to make sure numbers match
+print("\nBFZ Reporting Metrics")
+print("Actively Homeless ", filtered_df['Client ID'].where(filtered_df['Program End Date'].isnull()==True).nunique())
+print("Housing Placements ", filtered_df['Client ID'].where(filtered_df['Dismissal Reason']=='Housed').nunique())
+print("Moved to Inactive ", filtered_df['Client ID'].where(filtered_df['Dismissal Reason']=='Inactive').nunique())
+def newly_identified_number():
+    nin = {}
+    for i in filtered_df['Client ID']:
+        if i not in nin:
+            nin[i] = False
+    for j in nin:
+        for k in filtered_df[filtered_df['Client ID']==j]['Date of Identification']:
+            if k.year == reporting_date.year and k.month == reporting_date.month:
+                nin[j] = True
+    nin_counter = 0
+    for x in nin:
+        if x == True:
+            nin_counter += 1
+    return nin_counter
+print("Newly Identified Inflow ",newly_identified_number())
+
+def housing_lot():
+    housed = filtered_df[filtered_df['Dismissal Reason']=='Housed']
+    lot = [(housed.loc[i,'Program End Date'] - housed.loc[i,'Date of Identification']) for i in housed.index]
+    average = 0
+    for i in lot:
+        average += i.days
+    return average/len(lot)
+print("Average Length of Time from ID to Housing Placement ",housing_lot())
